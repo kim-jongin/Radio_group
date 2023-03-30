e2187b00e958bbdd149b4f3a8a13bab0bdaa89b4
kim-jongin <whddlskss@gmail.com>
2023-03-30 17:32:36 +0900

    [feat] RootWrap

--- radio-group/src/App.js
+++ radio-group/src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import styled from 'styled-components';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <RootWrap></RootWrap>;
 }
 
 export default App;
+
+const RootWrap = styled.div`
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  width: 100%;
+  max-width: 500px;
+
+  left: 50%;
+  transform: translate(-50%, 0);
+
+  background-color: white;
+`;
